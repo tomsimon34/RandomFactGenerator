519b628a81a8998f44236461e22ceb3a27fb5ddc
AhmedR236 <ahmed.r2365@gmail.com>
2025-04-08 16:32:54 -0400

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const URL = 'https://api.api-ninjas.com/v1/facts';
 
-
 function App() {
-
   const [fact, setFact] = useState('');
 
   useEffect(() => {
@@ -18,25 +16,19 @@ function App() {
         'X-Api-Key': process.env.REACT_APP_NINJAS_API
       }
     });
-    
-    const data = await response.json();
 
+    const data = await response.json();
     setFact(data[0].fact);
   }
 
   return (
-    <div>
-      <p>
-        "{fact}"
-      </p>
-      <button onClick={getRandomFact}>
-        Generate Random Fact
-      </button>
+    <div className="App">
+      <header className="App-header"> {/* Apply styles here */}
+        <p>"{fact}"</p>
+        <button onClick={getRandomFact}>Generate Random Fact</button>
+      </header>
     </div>
-  )
-  
-
+  );
 }
-
 
 export default App;
